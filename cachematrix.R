895d9ee2e6cea935ed30af1fd47f70ec1a8b38d5
Paula Palomino <52421886+paulatpalomino@users.noreply.github.com>
2021-02-25 10:20:38 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a matrix object that can cache its own inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+rm(list = ls())
 
+makeCacheMatrix <- function(ma = matrix()) {
+  im <- NULL
+  setMatrix <- function(y) {
+    ma <<- y
+    im <<- NULL
+  }
+  getMatrix <- function() ma
+  setinverse <- function(inv) im <<- inv
+  getinverse <- function() im
+  list(setMatrix = setMatrix,
+       getMatrix = getMatrix,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the matrix returned by the first function. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  im <- x$getinverse()
+  if (!is.null(im)) {
+    message("get cached inverse matrix")
+    return(im)
+  }
+  data <- x$getMatrix()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+        ## Return a matrix that is the inverse of 'M'
+}
+M <- matrix(c(1,2,3,4),2,2)
+
+M1 <- makeCacheMatrix(M)
+cacheSolve(M1) #inverse returned after computation with no message
+
+cacheSolve(M1) #inverse returned from cache with message printed here
+
+M2 <- makeCacheMatrix(-M)
+cacheSolve(M1)
+cacheSolve(M2)
